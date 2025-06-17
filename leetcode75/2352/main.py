fba220e9eb6fe1ddff402b9857f7bbe0bff8361c
tanum-3367 <uong.minh.tan@sun-asterisk.com>
2025-06-17 12:57:00 +0900

    complete day28

--- leetcode75/2352/main.py
+++ leetcode75/2352/main.py
@@ -14,3 +14,9 @@ def equalPairs(self, grid):
             i += 1
         j += 1
         i = 0
+    pair_count = 0
+    for i in range(n):
+        for j in range(n):
+            if grid[i] == columnGrid[j]:
+                pair_count += 1
+    return pair_count
