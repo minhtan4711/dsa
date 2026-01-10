5f32924e984bf7c3e644e0bc7c29661943b448de
minhtan <32776120+minhtan4711@users.noreply.github.com>
2026-01-10 19:10:41 +0700

    add another approach using two pointers

--- leetcode75/2095_delete_the_middle_node_in_linked_list/main.py
+++ leetcode75/2095_delete_the_middle_node_in_linked_list/main.py
@@ -9,6 +9,7 @@ class LinkedList:
         self.data = data
         self.next = None
 
+# brute force
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # node count
@@ -34,3 +35,31 @@ class Solution:
         prev_node.next = middle_node.next
         middle_node.next = None
         return head
+
+# two pointer
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class LinkedList:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # two pointer
+        while head is None or head.next is None:
+            return None
+
+        slow, fast = head, head.next.next
+
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        slow.next = slow.next.next
+        return head
+
+
